cd066f399ad73416286984506ab1e364b5368fe8
Alireza Badrloo <arbadrloo@gmail.com>
2023-03-16 00:49:10 +0330

    added class "NonMetalElement".
    overrode "Element" methods. method "describeElement" got its specific procedure.

---
+++ src/NonMetalElement.java
@@ -0,0 +1,26 @@
+public class NonMetalElement extends Element{
+
+    public NonMetalElement(String symbol, String atomicNumber, float atomicWeight) {
+        super(symbol, atomicNumber, atomicWeight);
+    }
+    @Override
+    public String getSymbol() {
+        return super.getSymbol();
+    }
+    @Override
+    public String getAtomicNumber() {
+        return super.getAtomicNumber();
+    }
+
+    @Override
+    public float getAtomicWeight() {
+        return super.getAtomicWeight();
+    }
+    @Override
+    public String describeElement() {
+        return "this is a Nonmetal element. Nonmetals are not good conductors of heat and electricity \n"
+                + "Element Symbol: " + this.symbol + "\n"
+                + "Element Atomic Number: " + this.atomicNumber + "\n"
+                + "Element Atomic Weight: " + this.atomicWeight + "\n";
+    }
+}
